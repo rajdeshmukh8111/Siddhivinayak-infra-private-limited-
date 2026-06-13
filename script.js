e47186d2f6065d4910a16880562b19cdbee53523
rajdeshmukh8111 <rajdeshmukh111d@gmail.com>
2026-06-13 08:59:51 +0530

    script.js

--- script.js
+++ script.js
@@ -88,3 +88,38 @@ reveals[i].classList.add("active");
 }
 
 }
+// ANIMATED COUNTERS
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+counter.innerText = "0";
+
+const updateCounter = () => {
+
+const target = +counter.getAttribute("data-target");
+
+const c = +counter.innerText;
+
+const increment = target / 100;
+
+if(c < target){
+
+counter.innerText = `${Math.ceil(c + increment)}`;
+
+setTimeout(updateCounter,20);
+
+}
+
+else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCounter();
+
+});
