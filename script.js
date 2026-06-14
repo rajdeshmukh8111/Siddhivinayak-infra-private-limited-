f063fa1060b353a37503b0d5d5117c3133b2b65f
rajdeshmukh8111 <rajdeshmukh111d@gmail.com>
2026-06-14 23:45:36 +0530

    script.js

--- script.js
+++ script.js
@@ -185,36 +185,4 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
         console.log(error);
     });
 });
-const counters=document.querySelectorAll('.counter');
 
-counters.forEach(counter=>{
-
-counter.innerText='0';
-
-const updateCounter=()=>{
-
-const target=+counter.getAttribute('data-target');
-
-const count=+counter.innerText;
-
-const increment=target/100;
-
-if(count<target){
-
-counter.innerText=Math.ceil(count+increment);
-
-setTimeout(updateCounter,20);
-
-}
-
-else{
-
-counter.innerText=target;
-
-}
-
-};
-
-updateCounter();
-
-});
