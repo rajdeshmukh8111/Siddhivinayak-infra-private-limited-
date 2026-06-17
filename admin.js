42b55bfb070ff36f0926f1ce72f0f0a77821dee1
rajdeshmukh8111 <rajdeshmukh111d@gmail.com>
2026-06-18 00:10:43 +0530

    admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,27 @@
+function login() {
 
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+if (
+(username === "Rajababu" &&
+password === "siddhivinayakinfra@111")
+
+||
+
+(username === "Admin" &&
+password === "staff@23")
+){
+
+window.location.href = "dashboard.html";
+
+}
+
+else{
+
+document.getElementById("error").innerText =
+"Invalid Username or Password";
+
+}
+
+}
