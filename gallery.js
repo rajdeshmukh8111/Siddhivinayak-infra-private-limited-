b290d9d75160b4f34e8889a03cba304180d94d97
rajdeshmukh8111 <rajdeshmukh111d@gmail.com>
2026-06-18 00:21:05 +0530

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,1 +1,17 @@
+const galleryGrid = document.getElementById("galleryGrid");
 
+const latestImage = localStorage.getItem("latestImage");
+
+if(latestImage){
+
+const div = document.createElement("div");
+
+div.className = "gallery-item";
+
+div.innerHTML = `
+<img src="${latestImage}">
+`;
+
+galleryGrid.prepend(div);
+
+}
