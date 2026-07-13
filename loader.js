030e910f78ecd17a381cc5fa78505e65409e0278
rajdeshmukh8111 <rajdeshmukh111d@gmail.com>
2026-07-14 00:21:04 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,17 @@
+window.onload=function(){
 
+setTimeout(function(){
+
+document.body.style.opacity="0";
+
+document.body.style.transition="1s";
+
+setTimeout(function(){
+
+window.location.href="index.html";
+
+},1000);
+
+},3500);
+
+};
